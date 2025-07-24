9e200a9db88d0fd8f300bca05af6bb68bb811899
Ioamra <iom.monchau@gmail.com>
2025-07-24 16:31:01 +0200

    Refactor le modal de connexion pour utiliser Reactive Forms et améliorer la gestion des erreurs

--- client/src/app/components/modals/login-modal/login-modal.component.ts
+++ client/src/app/components/modals/login-modal/login-modal.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthStore } from '../../../stores/auth.store';
 import { TextInputComponent } from '../../inputs/text-input/text-input.component';
 import { BaseModalComponent } from '../base-modal/base-modal.component';
@@ -8,7 +8,7 @@ import { BaseModalComponent } from '../base-modal/base-modal.component';
 @Component({
   selector: 'app-login-modal',
   standalone: true,
-  imports: [BaseModalComponent, CommonModule, FormsModule, TextInputComponent],
+  imports: [BaseModalComponent, CommonModule, ReactiveFormsModule, TextInputComponent],
   templateUrl: './login-modal.component.html',
   styleUrl: './login-modal.component.scss',
 })
@@ -17,22 +17,40 @@ export class LoginModalComponent {
   @Output() closeModal = new EventEmitter<void>();
 
   private readonly authStore = inject(AuthStore);
+  private readonly fb = inject(FormBuilder);
 
-  loginData = {
-    email: '',
-    password: '',
-  };
+  loginForm: FormGroup;
+
+  constructor() {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
 
   onClose(): void {
+    this.loginForm.reset();
     this.closeModal.emit();
   }
 
   onSubmit(): void {
-    if (this.loginData.email && this.loginData.password) {
+    if (this.loginForm.valid) {
       // Ici vous pouvez appeler votre service d'authentification
-      console.log('Login attempt:', this.loginData);
-      // this.authStore.login(this.loginData);
+      // const loginData = this.loginForm.value;
+      // this.authStore.login(loginData);
       this.onClose();
+    } else {
+      // Marquer tous les champs comme touchés pour afficher les erreurs
+      this.loginForm.markAllAsTouched();
     }
+  }
+
+  // Méthodes utilitaires pour accéder facilement aux contrôles
+  get email(): AbstractControl | null {
+    return this.loginForm.get('email');
+  }
+
+  get password(): AbstractControl | null {
+    return this.loginForm.get('password');
   }
 }
